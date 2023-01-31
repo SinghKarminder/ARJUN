179bf203cae63d737109892e4bc0e4684a3e4f00
Vinay Yadav <96142751+vnydv21@users.noreply.github.com>
2023-01-31 18:56:58 +0530

    Update cam.py

--- cam.py
+++ cam.py
@@ -92,7 +92,7 @@ class ImageCapture:
             pass
 
         now = datetime.now()
-        img_name = f'{now.strftime("%d-%m-%Y_%H-%M-%S")}_{DEVICE_SERIAL_ID}.mjpg'
+        img_name = f'{now.strftime("%d-%m-%Y_%H-%M-%S")}_{DEVICE_SERIAL_ID}.jpg'
         cv.imwrite(BUFFER_IMAGES_PATH+img_name, self._curImg)
 
         log.info("Image Saved -> " + img_name)
